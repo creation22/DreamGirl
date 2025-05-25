d350f31d02ab1fc910085ea32ee02b72c02808e6
creation <creation2224@gmail.com>
2025-05-25 22:53:37 +0530

    hero section done

---
+++ DreamGirl/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { TypewriterEffectDemo } from './components/TypeWriterUse'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    
+    <TypewriterEffectDemo/>
+    
+
+    </>
+  )
+}
+
+export default App
---
+++ DreamGirl/src/components/TypeWriterUse.jsx
@@ -0,0 +1,36 @@
+"use client";
+import { TypewriterEffect } from "./typewriter-effect";
+
+export function TypewriterEffectDemo() {
+const words = [
+  { text: "Find\u00A0" },
+  { text: "how\u00A0" },
+  { text: "your\u00A0" },
+  { text: "dream\u00A0" },
+  { text: "girl\u00A0" },
+  { text: "looks\u00A0" },
+  { text: "with\u00A0" },
+  {
+    text: "DreamHer ",
+    className: "text-blue-500 dark:text-blue-500 ",
+  },
+];
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-b from-pink-500 to-black ...">
+      <p className="text-neutral-600 dark:text-neutral-200 text-2xl  mb-10   font-extralight">
+        Describe Her. We'll Draw Her 🥰
+      </p>
+      <TypewriterEffect words={words} />
+      <div
+        className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4 mt-10">
+          
+        <button
+          className="w-40 h-10 rounded-xl bg-white text-black border border-black  text-sm">
+          Lets Find
+        </button>
+        
+      </div>
+    </div>
+  );
+}
